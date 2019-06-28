321765e38ad0a40b698cdb8e21abad107dd90393
Arthkrat <42123435+Arthkrat@users.noreply.github.com>
2019-06-28 17:32:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 $(function(){
-$('.modalOpen').on('click', function(e){
-    e.preventDefault();
-$('.layer').slideDown(500, function(){
-    $('.bottom').css({
-        display: 'none'
-    }).addClass('modal').slideDown(500);
-});/*300, function(){
-    $(this).on('click', ()=>$(this).hide());
-});*/
-});
-$('.layer').on('click', ()=>{
-    $('.bottom').slideUp(500, ()=>$('.layer').slideUp(500, ()=>$('.bottom').removeClass('modal').css({display: 'flex'})));//.removeClass('modal').hide(500,()=>$('.layer').hide(500));
-    
-    
-})
-
-
+    $('.modalOpen').on('click', function(e){
+        e.preventDefault();
+        $('.layer').slideDown(500, function(){
+            $('.bottom').css({
+                display: 'none'
+            }).addClass('modal').slideDown(500);
+        });
+    });
+    $('.layer').on('click', ()=>{
+        $('.bottom').slideUp(500, ()=>$('.layer').slideUp(500, ()=>$('.bottom')
+        .removeClass('modal').css({display: 'flex'})));     
+    })
 });
